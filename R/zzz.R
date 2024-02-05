d996d01f60cf121f22a38a04bebd056e43ea893a
gertstulp <g.stulp@rug.nl>
2024-02-05 08:51:24 +0100

    updated citation plus loading message

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("When you use this package in your work, please cite:\nStulp, G. (2023). Describing the Dutch Social Networks and Fertility Study and how to process it. Demographic Research 49(19), 493-512. https://www.demographic-research.org/articles/volume/49/19")
+}
